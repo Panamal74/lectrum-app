3cb465c815f774b6744d1c56b6517139acec7ee6
Panamal74 <pavlo.malakhovskyi@gmail.com>
2018-05-05 13:54:25 +0300

    Lessons 1.1

---
+++ source/index.js
@@ -0,0 +1,15 @@
+// Core
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Theme
+import "./theme/init";
+
+// App
+//import App from "./containers/App";
+
+//const H1 = React.createElement('h1', { title: 'The title' }, 'Hello lectrum');
+
+const H1JSX = <h1 title = 'The title'>Hello JSX lectrum</h1>;
+
+ReactDOM.render(H1JSX, document.getElementById("app"));
