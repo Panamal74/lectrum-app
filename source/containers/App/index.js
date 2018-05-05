54de03e54288ac4e46c83fb111594d68ebec8b64
Panamal74 <pavlo.malakhovskyi@gmail.com>
2018-05-05 14:07:58 +0300

    Lessons 1.2

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -4,18 +4,10 @@ import React, { Component } from 'react';
 export default class App extends Component {
     render () {
         return (
-            <h1
-                style = { {
-                    display:         'flex',
-                    justifyContent:  'center',
-                    alignItems:      'center',
-                    height:          '100vh',
-                    backgroundColor: '#090C14',
-                    color:           '#FFF',
-                    fontSize:        30,
-                } }>
-                Привет! Начало здесь.
-            </h1>
+            <>
+                <div>Hello lectrum</div>
+                <div>I am a student</div>
+            </>
         );
     }
 }
--- source/index.js
+++ source/index.js
@@ -6,10 +6,11 @@ import ReactDOM from "react-dom";
 import "./theme/init";
 
 // App
-//import App from "./containers/App";
+import App from "./containers/App";
 
 //const H1 = React.createElement('h1', { title: 'The title' }, 'Hello lectrum');
 
-const H1JSX = <h1 title = 'The title'>Hello JSX lectrum</h1>;
+//const H1JSX = <h1 title = 'The title'>Hello JSX lectrum</h1>;
 
-ReactDOM.render(H1JSX, document.getElementById("app"));
+//ReactDOM.render(H1JSX, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
